c86c8e83b64f9b9e64665caadc218cb3a12a0242
Matheus Passos <matheushtip@gmail.com>
2022-02-15 17:19:24 -0300

    suport to test from multiple ip sources

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import socket
-
 from speedtest import Speedtest
 from datetime import datetime
 from report import Report
 import sys
-import _thread
+import threading
 
 
 def choose_best_server(test: Speedtest) -> Speedtest:
@@ -17,13 +15,19 @@ def choose_best_server(test: Speedtest) -> Speedtest:
 
 
 def perform_test(test: Speedtest):
+    print("was called")
     moment = datetime.now().strftime('%d-%m-%Y, %H:%M:%S')
     download_result = test.download()
     upload_result = test.upload()
-    download_result_mbits = download_result / 1048576
-    upload_result_mbits = upload_result / 1048576
+    download_result_mbits = download_result / 1000000
+    upload_result_mbits = upload_result / 1000000
 
-    return {'moment': moment, 'upload': f'{upload_result_mbits:.3f}', 'download': f'{download_result_mbits:.3f}'}
+    return {
+            'moment': moment,
+            'source_ip': test._source_address,
+            'upload': f'{upload_result_mbits:.3f}',
+            'download': f'{download_result_mbits:.3f}'
+           }
 
 
 def get_keys(a_list: list):
@@ -32,24 +36,38 @@ def get_keys(a_list: list):
 
 
 def perform_tests(source: str):
+    print("was called")
     test = Speedtest(source_address=source)
     test = choose_best_server(test)
-
     a_list = []
-    _thread.start_new_thread(get_keys, (a_list,))
+    input_threading = threading.Thread(target=get_keys, args=(a_list,))
+    input_threading.start()
+
     while not a_list:
         yield perform_test(test)
 
 
-if __name__ == '__main__':
+def setup():
     if len(sys.argv) > 1:
-        SOURCE_IP = sys.argv[1]
+        source_ips = sys.argv[1:]
+        for source_ip in source_ips:
+            testing = threading.Thread(target=invoke_testing,
+                                       args=(source_ip,))
+            testing.start()
+            print("started...")
+        print(sys.argv)
     else:
         raise Exception("Error no source ip provided!")
 
-    report = Report()
+
+def invoke_testing(source_ip: str):
+    print("was called")
+    report = Report(source_ip=source_ip)
     row = 1
-    for result_dict in perform_tests(SOURCE_IP):
+    for result_dict in perform_tests(source_ip):
         row += 1
         report.write_file(result_dict)
         print(result_dict)
+
+
+setup()
--- report.py
+++ report.py
@@ -5,13 +5,15 @@ from datetime import datetime
 
 
 class Report:
-    def __init__(self, filepath: str = None):
-        self.filepath = path.join(curdir, f'{str(date.today().strftime("%B"))}.csv')
+    def __init__(self, filepath: str = None, source_ip: str = "no-source-ip"):
+        self.filepath = path.join(curdir,
+                                  f'{source_ip}'
+                                  '-{str(date.today().strftime("%B"))}.csv')
 
     def write_file(self, info: dict):
         is_file_new = not path.exists(self.filepath)
         with open(self.filepath, 'a') as csv_file:
-            fieldnames = ['moment', 'upload', 'download']
+            fieldnames = ['moment', 'source_ip', 'upload', 'download']
             report_writer = csv.DictWriter(csv_file, fieldnames)
             if is_file_new:
                 report_writer.writeheader()
